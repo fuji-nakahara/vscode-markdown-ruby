9833f7c6d9bbb7cd2d26be38dcf47cce0a2bd033
Fuji Nakahara <fujinakahara2032@gmail.com>
2018-07-15 23:11:04 +0900

    Remove unnecessary examples

--- extension.js
+++ extension.js
@@ -10,17 +10,6 @@ function activate(context) {
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "markdown-ruby" is now active!');
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', function () {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
-
-    context.subscriptions.push(disposable);
 }
 exports.activate = activate;
 
