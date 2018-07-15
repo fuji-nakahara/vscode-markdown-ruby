5d7dc99d88aaf2b4dab9ffa7d2a6a2a7dc100c18
Fuji Nakahara <fujinakahara2032@gmail.com>
2018-07-15 23:21:56 +0900

    Implement markdown extention using 'markdown-it-ruby'

--- extension.js
+++ extension.js
@@ -1,19 +1,8 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
-
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-function activate(context) {
-
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "markdown-ruby" is now active!');
-
+function activate() {
+    return {
+        extendMarkdownIt(md) {
+            return md.use(require('markdown-it-ruby'));
+        }
+    };
 }
 exports.activate = activate;
-
-// this method is called when your extension is deactivated
-function deactivate() {
-}
-exports.deactivate = deactivate;
